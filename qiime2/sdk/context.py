8dace9ad5556f413f527ede60cf0c25c94530b30
Oddant1 <oddant1@hotmail.com>
2024-11-19 13:38:26 -0700

    Refactor

--- qiime2/sdk/context.py
+++ qiime2/sdk/context.py
@@ -53,10 +53,18 @@ class Context:
             # The function is the first arg, we ditch that
             args = args[1:]
 
-            # Check for cached results to recycle
-            if cached_results := self._check_cache(
-                    args, kwargs, action_obj, plugin_action):
-                return cached_results
+            # If we have a named_pool, we need to check for cached results that
+            # we can reuse.
+            #
+            # We can short circuit our index checking if any of our arguments
+            # are proxies because if we got a proxy as an argument, we know it
+            # is a new thing we are computing from a prior step in the pipeline
+            # and thus will not be cached.
+            if self.cache.named_pool is not None and \
+                    not self._contains_proxies(*args, **kwargs) and \
+                        (cached_results := self._check_cache(
+                            args, kwargs, action_obj, plugin_action)):
+                    return cached_results
 
             # If we didn't have cached results to reuse, we need to execute
             # the action.
@@ -72,37 +80,28 @@ class Context:
         return action_obj._bind(lambda: self)(*args, **kwargs)
 
     def _check_cache(self, args, kwargs, action_obj, plugin_action):
-        # If we have a named_pool, we need to check for cached results that
-        # we can reuse.
-        #
-        # We can short circuit our index checking if any of our arguments
-        # are proxies because if we got a proxy as an argument, we know it
-        # is a new thing we are computing from a prior step in the pipeline
-        # and thus will not be cached.
-        if self.cache.named_pool is not None and \
-                not self._contains_proxies(*args, **kwargs):
+        # Type management for inputs
+        collated_inputs = action_obj.signature.collate_inputs(
+            *args, **kwargs)
+        callable_args = action_obj.signature.coerce_user_input(
+            **collated_inputs)
 
-            collated_inputs = action_obj.signature.collate_inputs(
-                *args, **kwargs)
-            callable_args = action_obj.signature.coerce_user_input(
-                **collated_inputs)
+        # Make args and kwargs look how they do when we read them
+        # out of a .yaml file (list of single value dicts of
+        # input_name: value)
+        arguments = []
+        for k, v in callable_args.items():
+            arguments.append({k: v})
 
-            # Make args and kwargs look how they do when we read them
-            # out of a .yaml file (list of single value dicts of
-            # input_name: value)
-            arguments = []
-            for k, v in callable_args.items():
-                arguments.append({k: v})
-
-            invocation = HashableInvocation(plugin_action, arguments)
-            if invocation in self.cache.named_pool.index:
-                # It is conceivable that since we created our index the
-                # pool we indexed has been destroyed. If that is the
-                # case we want to just continue on and rerun the action
-                try:
-                    return self._load_cache(action_obj, invocation)
-                except KeyError:
-                    pass
+        invocation = HashableInvocation(plugin_action, arguments)
+        if invocation in self.cache.named_pool.index:
+            # It is conceivable that since we created our index the
+            # pool we indexed has been destroyed. If that is the
+            # case we want to just continue on and rerun the action
+            try:
+                return self._load_cache(action_obj, invocation)
+            except KeyError:
+                pass
 
     def _load_cache(self, action_obj, invocation):
         """Load cached results
