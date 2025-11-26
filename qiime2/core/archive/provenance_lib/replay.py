75989d3c9d84d1b35e75a651408be2f22093477e
Oddant1 <oddant1@hotmail.com>
2025-11-26 13:54:39 -0700

    Remove dead param

--- qiime2/core/archive/provenance_lib/replay.py
+++ qiime2/core/archive/provenance_lib/replay.py
@@ -849,8 +849,7 @@ def build_action_usage(
 
     usg_var = cfg.use.action(
         cfg.use.UsageAction(
-            plugin_id=plugin, action_id=action, node=node,
-            action_present=action_present),
+            plugin_id=plugin, action_id=action, node=node),
         cfg.use.UsageInputs(**inputs),
         cfg.use.UsageOutputNames(**outputs)
     )
