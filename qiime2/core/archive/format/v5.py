9db97fdeb08b913eae7e53e474978d5f3ebbfd6f
Colin Wood <68213641+colinvwood@users.noreply.github.com>
2025-12-23 11:28:58 -0700

    Checksum caching (#885)
    Cache checksums to avoid running expensive checksum calculations more than needed

--- qiime2/core/archive/format/v5.py
+++ qiime2/core/archive/format/v5.py
@@ -6,8 +6,12 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
+from pathlib import Path
+import os
+
 import qiime2.core.archive.format.v4 as v4
-from qiime2.core.archive.format.util import write_checksums
+from qiime2.core.util import to_checksum_format, checksum
+from qiime2.sdk.result import ChecksumCache
 
 
 class ArchiveFormat(v4.ArchiveFormat):
@@ -25,8 +29,41 @@ class ArchiveFormat(v4.ArchiveFormat):
 
     @classmethod
     def write_checksums(cls, archive_record):
-        write_checksums(
-            directory=str(archive_record.root),
-            checksum_file=cls.CHECKSUM_FILE,
-            checksum_type=cls.CHECKSUM_TYPE
-        )
+        checksum_cache = ChecksumCache()
+
+        checksums = {}
+        for root, dirs, files in os.walk(archive_record.root, topdown=True):
+            dirs[:] = sorted([d for d in dirs if not d[0] == '.'])
+            for file in sorted(files):
+                if file[0] == '.':
+                    continue
+
+                path = Path(root) / file
+                archive_relative_path = path.relative_to(
+                    Path(archive_record.root)
+                )
+
+                # always ignore annotations dir when checksumming
+                # so we dont self-invalidate when adding/removing annotations
+                if archive_relative_path.is_relative_to(Path('annotations')):
+                    continue
+
+                # check checksum cache
+                checksum_string = None
+                if archive_relative_path.is_relative_to(
+                    Path("provenance/artifacts")
+                ):
+                    cached_path = archive_relative_path.relative_to(
+                        Path("provenance/artifacts")
+                    )
+                    checksum_string = checksum_cache.get(cached_path)
+
+                if checksum_string is None:
+                    checksum_string = checksum(str(path), cls.CHECKSUM_TYPE)
+
+                checksums[str(archive_relative_path)] = checksum_string
+
+        with (Path(archive_record.root) / cls.CHECKSUM_FILE).open('w') as fh:
+            for item in checksums.items():
+                fh.write(to_checksum_format(*item))
+                fh.write('\n')
