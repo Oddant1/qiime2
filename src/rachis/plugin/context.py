2228e358ac983b640a556a30328db94c14dbcb66
Oddant1 <oddant1@hotmail.com>
2026-03-25 14:10:23 -0700

    Remove non plugin dev facing methods from IContext

--- src/rachis/plugin/context.py
+++ src/rachis/plugin/context.py
@@ -10,30 +10,10 @@ import abc
 
 class IContext(metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    def _dispatch_(self, args, kwargs):
-        """Submit an action for execution
-        """
-
-    @abc.abstractmethod
     def get_action(self, plugin: str, action: str):
         """Return a function matching the callable API of an action.
         This function is aware of the pipeline context and manages its own
         cleanup as appropriate.
-        """
-
-    @abc.abstractmethod
-    def _callable_action_(self, *args, **kwargs):
-        """The actual executable called when running an action
-        """
-
-    @abc.abstractmethod
-    def _check_cache(self, args, kwargs):
-        """Check if a given Result is in the cache
-        """
-
-    @abc.abstractmethod
-    def _load_cache(self, invocation):
-        """Load cached results
         """
 
     @abc.abstractmethod
@@ -48,15 +28,4 @@ class IContext(metaclass=abc.ABCMeta):
     def make_report(self, template, collection):
         """Create a report based on a template and a collection of
             visualizations
-        """
-
-    # NOTE: We end up with both the artifact and the pipeline alias of artifact
-    # in the named cache in the end. We only have the pipeline alias in the
-    # process pool
-    @abc.abstractmethod
-    def add_reference(self, ref):
-        """Add a reference to something destructable that will be owned by the
-           parent scope. The reason it needs to be tracked is so that on
-           failure, a context can still identify what will (no longer) be
-           returned.
         """
