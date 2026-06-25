451e5a9ccbdab94522bede74b2aad90600526fd8
Oddant1 <oddant1@hotmail.com>
2026-06-25 16:19:04 -0700

    test

--- src/rachis/core/archive/provenance_lib/tests/test_replay.py
+++ src/rachis/core/archive/provenance_lib/tests/test_replay.py
@@ -263,6 +263,26 @@ class ReplayProvenanceTests(unittest.TestCase):
             self.assertNotIn('optional1=', rendered)
             self.assertNotIn('num2=', rendered)
 
+    def test_replay_imported_artifact(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            out_fp = pathlib.Path(tmpdir) / 'rendered.txt'
+            in_fp = self.das.single_int.filepath
+
+            replay_provenance(
+                ReplayPythonUsage, in_fp, out_fp, md_out_dir=tmpdir
+            )
+            self.assertTrue(out_fp.is_file())
+
+            with open(out_fp, 'r') as fp:
+                rendered = fp.read()
+
+            self.assertIn('from rachis import Artifact', rendered)
+            self.assertIn('single_int_0 = Artifact.import_data(', rendered)
+            self.assertIn("'SingleInt',", rendered)
+            self.assertIn('<your data here>,', rendered)
+            self.assertIn(')', rendered)
+            self.assertIn("single_int_0.save('single_int_0')", rendered)
+
 
 class MultiplePluginTests(unittest.TestCase):
     @classmethod
